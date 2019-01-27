530db0d811236c693a30ca287c47b32e6bbd95a2
roceleylaw <roceleylaw@gmail.com>
2019-01-26 22:34:03 -0800

    A1: process management: added pcb table init function

--- c/pcb.c
+++ c/pcb.c
@@ -1,11 +1,11 @@
 /* PCB implementation */
 #include <xeroskernel.h>
 
-extern pcb_t *stopped_queue;
-extern pcb_t *ready_queue;
-extern pcb_t *blocked_queue;
+pcb_t *stopped_queue;
+pcb_t *ready_queue;
+pcb_t *blocked_queue;
 // for debugging purpose
-extern pcb_t *running_process;
+pcb_t *running_process;
 
 extern void initpcbtable() {
     kprintf("/n === initializing pcb table ======= /n");
