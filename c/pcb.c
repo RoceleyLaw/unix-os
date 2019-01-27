5b7e1c69a21b214ed4aa83373f3ac5c1f23e1cbe
roceleylaw <roceleylaw@gmail.com>
2019-01-26 22:50:36 -0800

    A1: process management: added pcb table init function; fixed make issue; workcopy1

--- c/pcb.c
+++ c/pcb.c
@@ -8,9 +8,19 @@ pcb_t *blocked_queue;
 pcb_t *running_process;
 
 extern void initpcbtable() {
-    kprintf("/n === initializing pcb table ======= /n");
+    kprintf("\n === initializing pcb table ======= \n");
     running_process = NULL;
     blocked_queue = NULL;
     size_t pcbtable_size = sizeof(pcb_table) / sizeof (pcb_t);
-    kprintf("/n ##### pcb table size: %d ####### /n", pcbtable_size);
+    kprintf("\n ##### pcb table size: %d ####### \n", pcbtable_size);
+
+    pcb_t *cur = stopped_queue;
+    for(int i = 0; i < pcbtable_size; i++) {
+        cur -> PID = i + 1;
+        cur -> state = STOPPED;
+        kprintf("\n ^^^^^ assuming dead process initialized in stoppped_queue=> PID: %d, cur: %d ^^", cur -> PID, cur);
+        cur = cur -> next;
+    }
+    // Avoid dangling ptr
+    cur = NULL;
 }
