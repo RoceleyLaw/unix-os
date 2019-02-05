d3c05f2044fe237c1274ed885caacf6e1e94ab17
Roceley Luo <roceleylaw@gmail.com>
2019-02-05 10:21:44 -0800

    calling dispatch

--- c/init.c
+++ c/init.c
@@ -72,7 +72,7 @@ void initproc( void )				/* The beginning */
   // Root proc
   kprintf("\n for root process.... ");
   create(root, 1024);
-  // dispatch();
+  dispatch();
 
   // create(NULL, 0);
   // create(NULL, 0);
