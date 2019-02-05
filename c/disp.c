c604c08175d90f198fdee48d21b092d30a6f2c69
Roceley Luo <roceleylaw@gmail.com>
2019-02-04 22:41:20 -0800

    try to fix next

--- c/disp.c
+++ c/disp.c
@@ -4,20 +4,6 @@
 #include <xeroskernel.h>
 
 /* Your code goes here */
-extern void dispatch() {
-    // process = next();
-    // for( ;; ) 
-    // request = contextswitch( process );
-    // switch( request ) {
-    //     case( CREATE ): create(new process) + place into the ready queue; break;
-    //     case( YIELD ): ready( process ); process = next() + remove the process from ready queue; break;
-    //     case( STOP ): cleanup( process ); process = next(); free the stack space and PCB; break; }
-    pcb_t* new_proc = &pcb_table[0];
-    enqueuepcb(READY,&pcb_table[0]);
-    pcb_t* process = (pcb_t*) next();
-    kprintf("NEXT PROCESS: %d", (int)process);
-}
-
 /* removes the next process from the ready queue 
    and returns an index or a pointer to its process control block. */
 pcb_t *next() {
@@ -33,4 +19,17 @@ void ready(pcb_t* pcb_ptr) {
 }
 
 void cleanup(pcb_t* pcb_ptr) {
+}
+extern void dispatch() {
+    // process = next();
+    // for( ;; ) 
+    // request = contextswitch( process );
+    // switch( request ) {
+    //     case( CREATE ): create(new process) + place into the ready queue; break;
+    //     case( YIELD ): ready( process ); process = next() + remove the process from ready queue; break;
+    //     case( STOP ): cleanup( process ); process = next(); free the stack space and PCB; break; }
+    pcb_t* new_proc = &pcb_table[0];
+    enqueuepcb(READY,&pcb_table[0]);
+    pcb_t* process = (pcb_t*) next();
+    kprintf("NEXT PROCESS: %d", (int)process);
 }
