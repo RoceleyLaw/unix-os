205e513acea9b95c020605cbf38a52aa6d6a3240
Roceley Luo <roceleylaw@gmail.com>
2019-02-05 16:58:39 -0800

    test ctsw

--- c/syscall.c
+++ c/syscall.c
@@ -4,28 +4,31 @@
 #include <xeroskernel.h>
 #include <stdarg.h>
 /* Your code goes here */
+static unsigned long process_id;
 extern int syscall( int call, ... ) {
 
     if (call == SYSCALL_CREATE) {
         va_list ap;
-        va_start(ap);
-        int result = 0;
-        __asm__ __volatile__("\
-            movl %1, %%eax \n\
-            movl %2, %%edx \n\
-            int $66 \n\
-            movl %%eax, %0 \n\
-        "
-        : "=r" (result)
-        :	"r" (call), "r" (ap)
-        : "%eax", "%edx"
-        );
+        va_start(ap, call);
+        void (*func)(void) = va_arg(ap, void(*)(void));
+        int stack = va_arg(ap, int);
         va_end(ap);
+        __asm__ __volatile__("\
+            movl %0, %%eax \n\
+            movl %1, %%ecx \n\
+            movl %2, %%edx \n\
+            int $49 \n\
+            movl %%eax, process_id \n\
+        "
+        : 
+        :"call"(call), "funcptr"(func), "stack"(stack)
+        : 
+        );
     }
-	return result;
+	return (int)process_id;
 }
 
-extern unsigned int syscreate( void (*func)(void), int stack ) {
+extern unsigned int syscreate(void (*func)(void), int stack ) {
     return syscall(SYSCALL_CREATE, (unsigned long)func,(unsigned long) stack);
 }
 extern void sysyield( void ) {
--- c/user.c
+++ c/user.c
@@ -11,19 +11,19 @@
 void root(void) {
     kprintf("Hello world!\n");
     //TODO: stack size is set randomly as 100, need to think about it
-    // syscreate(&producer, 100);
+    syscreate(&producer, 100);
     // syscreate(&consumer, 100);
     // for (;;) sysyield();
 }
 
 /* Producer */
-// void producer(void) {
-//     for (int i = 0; i < 15; i++) {
-//         kprintf("\n Happy 2019\n");
-//         sysyield();
-//     }
-//     sysstop();    
-// }
+void producer(void) {
+    for (int i = 0; i < 15; i++) {
+        kprintf("\n Happy 2019\n");
+        // sysyield();
+    }
+   // sysstop();    
+}
 
 // /* Consumer */
 // void consumer(void) {
